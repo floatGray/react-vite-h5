7d583e03765b3865b23c2cb0a2e52e15c248b3a1
floatgray <599316783@qq.com>
2023-03-14 14:06:11 +0800

    add css

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { createStyleImportPlugin } from "vite-plugin-style-import";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -27,6 +28,22 @@ export default defineConfig({
         // 支持内联 JavaScript
         javascriptEnabled: true,
       },
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        // 当遇到 /api 路径时，将其转换成 target 的值
+        target: "http://api.chennick.wang/api/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // 将 /api 重写为空
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // src 路径
+      utils: path.resolve(__dirname, "src/utils"), // src 路径
     },
   },
 });
