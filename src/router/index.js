fe346614fa7e91f5f6cd4e215c04c527aa945641
floatgray <599316783@qq.com>
2023-03-17 23:14:02 +0800

    add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,11 @@
 import Home from "@/container/Home";
 import Data from "@/container/Data";
 import User from "@/container/User";
-import Login from '@/container/Login'
+import Login from "@/container/Login";
+import Detail from "@/container/Detail";
+import Account from "@/container/Account";
+import About from "@/container/About";
+import UserInfo from "@/container/UserInfo";
 
 const routes = [
   {
@@ -19,6 +23,22 @@ const routes = [
   {
     path: "/login",
     component: Login,
+  },
+  {
+    path: "/detail",
+    component: Detail,
+  },
+  {
+    path: "/account",
+    component: Account,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+  {
+    path: "/userinfo",
+    component: UserInfo,
   },
 ];
 
